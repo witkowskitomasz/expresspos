93982cf941ce032883310185d60f0e136cea5dd1
Tomasz Witkowski <witkowskitomasz@protonmail.com>
2023-08-11 07:58:30 +0200

    another work on groups

--- static/js/config_ingredients.js
+++ static/js/config_ingredients.js
@@ -2,6 +2,42 @@ const groupList = document.querySelector('#group-list')
 const addBtn = document.querySelector('#add-btn')
 const groupInput = document.querySelector('#group')
 
+const groupNavBtn = document.querySelector('.groups-nav')
+const ingredientsNavBtn = document.querySelector('.ingredients-nav')
+const groupWindow = document.querySelector('.groups')
+const ingredientsWindow = document.querySelector('.ingredients')
+
+const showGroupWindow = () => {
+    if (!groupNavBtn.classList.contains('selected-btn') && !groupNavBtn.classList.contains('unselected-btn')) {
+        groupNavBtn.classList.add('selected-btn')
+        ingredientsNavBtn.classList.add('unselected-btn')
+        groupWindow.classList.add('show')
+    } else if (groupNavBtn.classList.contains('unselected-btn')) {
+        groupNavBtn.classList.remove('unselected-btn')
+        groupNavBtn.classList.add('selected-btn')
+        groupWindow.classList.add('show')
+        ingredientsNavBtn.classList.remove('selected-btn')
+        ingredientsNavBtn.classList.add('unselected-btn')
+        ingredientsWindow.classList.remove('show')
+    }
+}
+
+const showIngredientsWindow = () => {
+    if (!ingredientsNavBtn.classList.contains('selected-btn') && !ingredientsNavBtn.classList.contains('unselected-btn')) {
+        ingredientsNavBtn.classList.add('selected-btn')
+        groupNavBtn.classList.add('unselected-btn')
+        ingredientsWindow.classList.add('show')
+    } else if (ingredientsNavBtn.classList.contains('unselected-btn')) {
+        ingredientsNavBtn.classList.remove('unselected-btn')
+        ingredientsNavBtn.classList.add('selected-btn')
+        ingredientsWindow.classList.add('show')
+        groupNavBtn.classList.remove('selected-btn')
+        groupNavBtn.classList.add('unselected-btn')
+        groupWindow.classList.remove('show')
+    }
+}
+
+//Group ops
 const createGroupListElement = (name) => {
     const li = document.createElement('li')
     const div = document.createElement('div')
@@ -40,6 +76,10 @@ const addGroup = () => {
         createGroupList()
     }, 200)
 }
+//End
+
+groupNavBtn.addEventListener('click', () => showGroupWindow())
+ingredientsNavBtn.addEventListener('click', () => showIngredientsWindow())
 
 document.addEventListener('DOMContentLoaded', createGroupList())
 groupList.addEventListener('click', (e) => removeGroup(e))
